b78a981bffb0cc1ab78da94cbc6de5f7caf03998
ruanye <ye.ruan@qianmo-inc.com>
2016-12-22 11:43:49 +0800

    add route

--- src/main.js
+++ src/main.js
@@ -1,11 +1,22 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import VueRouter from 'vue-router';
 import App from './App';
+import Index from './pages/Index';
+
+Vue.use(VueRouter);
+
+const routes = [
+  { path: '/nroauth', component: Index, name: 'index' },
+];
+
+const router = new VueRouter({ mode: 'history', routes });
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  router,
   template: '<App/>',
   components: { App },
 });
---
+++ src/pages/Index.vue
@@ -0,0 +1,21 @@
+<template>
+  <p>Hello</p>
+  <!--<img src="../assets/logo.png">-->
+  <!--<div class="inputContainer">
+    <img src="../assets/logo.png">
+    <hello></hello>
+  </div>-->
+</template>
+
+<!--<script>
+  import Hello from '../components/Hello';
+
+  export default {
+    name: 'index',
+    components: {
+      Hello,
+    },
+  };
+</script>-->
+
+<!--<style></style>-->
